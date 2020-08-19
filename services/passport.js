c06cbc71060890df9a396d0def071aa7ea6f3870
young <young@MacBook-Pro.local>
2020-08-19 15:24:47 +0800

    tweak google strategies

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-const HttpsProxyAgent = require('https-proxy-agent');
+// const HttpsProxyAgent = require('https-proxy-agent');
 
 const User = mongoose.model('users');
 
@@ -20,7 +20,8 @@ passport.deserializeUser((id, done) => {
 const gStrategy = new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id })
         .then((existingUser) => {
@@ -33,7 +34,7 @@ const gStrategy = new GoogleStrategy({
             }
         })
 });
-const agent = new HttpsProxyAgent(process.env.HTTP_PROXY || 'http://127.0.0.1:9910');
-gStrategy._oauth2.setAgent(agent);
+// const agent = new HttpsProxyAgent(process.env.HTTP_PROXY || 'http://127.0.0.1:9910');
+// gStrategy._oauth2.setAgent(agent);
 
 passport.use(gStrategy);
